43a607d58209b48673f8556e9632403b777a1969
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-09 15:28:28 -0300

    refactor remove unused code

--- src/app/components/facilities/facilities.component.ts
+++ src/app/components/facilities/facilities.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Facility } from '../../Facility';
 import { FacilityService } from 'src/app/services/facility.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
