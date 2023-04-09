80cb457c81346e4f8331e16e7825132bcffee5b1
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-09 11:27:02 -0300

    change interface to hold null values as well

--- src/app/Facility.ts
+++ src/app/Facility.ts
@@ -2,7 +2,7 @@ export interface Facility {
   codigo_cnes: number;
   nome_razao_social: string;
   nome_fantasia: string;
-  numero_telefone_estabelecimento: string;
-  endereco_email_estabelecimento: string;
-  numero_cnpj: string;
+  numero_telefone_estabelecimento: string | null;
+  endereco_email_estabelecimento: string | null;
+  numero_cnpj: string | null;
 }
