a40b3c29230d612c3e8fe77f90275ed00f886285
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-09 14:11:23 -0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterFacilityComponent } from './pages/register-facility/register-facility.component';
+import { FacilitiesComponent } from './components/facilities/facilities.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FacilitiesComponent,
+  },
+  {
+    path: 'register-facility',
+    component: RegisterFacilityComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
