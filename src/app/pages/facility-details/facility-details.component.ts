8937ac4b801c2edbbb798c51bc129ee6ffe325b1
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-09 16:43:04 -0300

    add facility details page with map - working

--- src/app/pages/facility-details/facility-details.component.ts
+++ src/app/pages/facility-details/facility-details.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FacilityService } from 'src/app/services/facility.service';
+import { ActivatedRoute } from '@angular/router';
+import { Facility } from 'src/app/Facility';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-facility-details',
   templateUrl: './facility-details.component.html',
-  styleUrls: ['./facility-details.component.css']
+  styleUrls: ['./facility-details.component.css'],
 })
-export class FacilityDetailsComponent {
+export class FacilityDetailsComponent implements OnInit {
+  facility!: Facility;
+  loading: boolean = true;
 
+  constructor(
+    private facilityService: FacilityService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    const cnesCode: number = Number(this.route.snapshot.paramMap.get('cnes'));
+    this.facilityService.getFacilityInfoByCnes(cnesCode).subscribe((data) => {
+      this.facility = data;
+      this.loading = false;
+    });
+
+    if (!this.facility) {
+      this.loading = false;
+      alert(`Estabelecimento não encontrado com esse CNES ${cnesCode}`);
+    }
+  }
+
+  ngAfterViewInit() {
+    const map = L.map('map').setView(
+      [
+        this.facility.latitude_estabelecimento_decimo_grau ?? 0,
+        this.facility.longitude_estabelecimento_decimo_grau ?? 0,
+      ],
+      15
+    );
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([
+      this.facility.latitude_estabelecimento_decimo_grau ?? 0,
+      this.facility.longitude_estabelecimento_decimo_grau ?? 0,
+    ])
+      .addTo(map)
+      .bindPopup('Aqui está o estabelecimento.')
+      .openPopup();
+  }
 }
