f0280a159ae956ecd9d863847efb32af4e2fd42d
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-09 11:01:14 -0300

    add facility interface

---
+++ src/app/Facility.ts
@@ -0,0 +1,8 @@
+export interface Facility {
+  codigo_cnes: number;
+  nome_razao_social: string;
+  nome_fantasia: string;
+  numero_telefone_estabelecimento: string;
+  endereco_email_estabelecimento: string;
+  numero_cnpj: string;
+}
